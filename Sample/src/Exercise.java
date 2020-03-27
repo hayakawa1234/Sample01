73ca1404fd0d819ac24e19e516c0fd4da41eca51
hayakawa1234 <61731486+hayakawa1234@users.noreply.github.com>
2020-03-27 12:18:56 +0900

    トランプゲーム 12:18

--- Sample/src/Exercise.java
+++ Sample/src/Exercise.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise {
 
 	public static void main(String[] args) {
@@ -124,41 +127,61 @@ public class Exercise {
 
 			System.out.println(" ");
 			System.out.println("カードを5枚引いて最も強いカードを表示する");
-			Card drawcard1 = new Card(9, "dia");
+
+			Card drawcard1 = new Card(13, "dia");
 			Card drawcard2 = new Card(6, "spade");
-			Card drawcard3 = new Card(3, "dia");
-			
+			Card drawcard3 = new Card(7, "spade");
+			Card drawcard4 = new Card(10, "heart");
 
 			int num1_2 = drawcard1.compareTo(drawcard2);
 			System.out.println(num1_2);
 			int num1_3 = drawcard1.compareTo(drawcard3);
 			System.out.println(num1_3);
+			int num1_4 = drawcard1.compareTo(drawcard4);
+			System.out.println(num1_4);
 			int num2_1 = drawcard2.compareTo(drawcard1);
 			System.out.println(num2_1);
 			int num2_3 = drawcard2.compareTo(drawcard3);
 			System.out.println(num2_3);
+			int num2_4 = drawcard2.compareTo(drawcard4);
+			System.out.println(num2_4);
 			int num3_1 = drawcard3.compareTo(drawcard1);
 			System.out.println(num3_1);
 			int num3_2 = drawcard3.compareTo(drawcard2);
 			System.out.println(num3_2);
+			int num3_4 = drawcard3.compareTo(drawcard4);
+			System.out.println(num3_4);
+			int num4_1 = drawcard4.compareTo(drawcard1);
+			System.out.println(num4_1);
+			int num4_2 = drawcard4.compareTo(drawcard2);
+			System.out.println(num4_2);
+			int num4_3 = drawcard4.compareTo(drawcard3);
+			System.out.println(num4_3);
 
-			if ((num1_2 >= 0) && (num1_3 >= 0)) {
+			if ((num1_2 >= 0) && (num1_3 >= 0) && (num1_4 >= 0)) {
 				String number1 = drawcard1.getNumber();
 				String mark1 = drawcard1.getMark();
 				System.out.println(number1 + "," + mark1);
 			}
 
-			if ((num2_1 >= 0) && (num2_3 >= 0)) {
+			if ((num2_1 >= 0) && (num2_3 >= 0) && (num2_4 >= 0)) {
 				String number2 = drawcard2.getNumber();
 				String mark2 = drawcard2.getMark();
 				System.out.println(number2 + "," + mark2);
 			}
 
-			if ((num3_1 >= 0) && (num3_2 >= 0)) {
+			if ((num3_1 >= 0) && (num3_2 >= 0) && (num3_4 >= 0)) {
 				String number3 = drawcard3.getNumber();
 				String mark3 = drawcard3.getMark();
 				System.out.println(number3 + "," + mark3);
 			}
+
+			if ((num4_1 >= 0) && (num4_2 >= 0) && (num4_3 >= 0)) {
+				String number4 = drawcard4.getNumber();
+				String mark4 = drawcard4.getMark();
+				System.out.println(number4 + "," + mark4);
+			}
+
 		}
 	}
 }
